740cce19f3b24282120299a72e1ddc81cab7640c
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2019-03-29 20:04:36 +0300

    add more text to RankNTypes

--- Rank2Types.hs
+++ Rank2Types.hs
@@ -19,3 +19,19 @@ app r resp = do
 
 -- app' :: App
 -- app' r resp = return $ ??
+
+-- На самом деле App можно определить и без RankNTypes:
+
+type App' a = Request -> (Response -> IO a) -> IO a
+
+-- Но тогда forall придётся накинуть в определении функции, которая будет
+-- принимать приложение. Вот так:
+
+runApp' :: (forall a. App' a) -> IO ()
+runApp' f = undefined
+
+-- В то время как для предыдущего определения, которое само содержит forall,
+-- можно обойтись без этого:
+
+runApp :: App -> IO ()
+runApp f = undefined
